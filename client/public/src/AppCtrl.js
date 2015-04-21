878d913665439e8fdc06de9d91ae235db387935e
Luka <krsnik.luka92@gmail.com>
2015-04-21 19:02:39 +0200

    Continuing fixing errors

--- client/public/src/AppCtrl.js
+++ client/public/src/AppCtrl.js
@@ -1,7 +1,7 @@
 /*global angular */
 (function () {
     'use strict';
-    angular.module('scrumBan').controller('AppCtrl', 'AuthService', ['$scope', function ($scope, AuthService) {
+    angular.module('scrumBan').controller('AppCtrl', ['AuthService', '$scope', function ($scope, AuthService) {
         $scope.login = function () {
             AuthService.login($scope.user)
                 .success(function () {
@@ -10,10 +10,8 @@
                 .error(function (status) {
                     switch (status) {
                     case (400):
-                        {
                         $scope.loginForm.username.$setValidity('wrongCredentials', false);
                         break;
-                        }
                     }
                 });
         };
