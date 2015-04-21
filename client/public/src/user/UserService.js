5af6dc6bde548f4450c8c980f6a3232ed9f7fd22
rok7rupnik <rok7rupnik@gmail.com>
2015-04-21 19:50:03 +0200

    Working version of delete user Need to add confirmation popup

--- client/public/src/user/UserService.js
+++ client/public/src/user/UserService.js
@@ -12,6 +12,7 @@
                             console.log(error);
                         });
                 },
+
                 getUsers: function () {
                     return $http.get(API_URL + 'users/')
                         .error(function (error, status) {
@@ -23,6 +24,13 @@
                     return $http.post(API_URL + 'users/', user)
                         .error(function (error, status) {
                             console.log('Error in creating user: ' + status);
+                            console.log(error);
+                        });
+                },
+                deleteUser: function (userId) {
+                    return $http.delete(API_URL + 'users/' + userId + '/')
+                        .error(function (error, status) {
+                            console.log('Error in deleting user: ' + status);
                             console.log(error);
                         });
                 }
