711b808d14c825c340b84799763d053ac7a9842e
Luka <krsnik.luka92@gmail.com>
2015-05-11 00:02:32 +0200

    Created login.

--- server/app/views.py
+++ server/app/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from django.test import RequestFactory
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from app.serializers import UserSerializer, GroupSerializer
