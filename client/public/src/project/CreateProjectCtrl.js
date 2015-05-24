f066d10bd0fbe9466f9f361df7961cc23f821a4f
mancazerovnik <manca.zerovnik3@gmail.com>
2015-05-24 22:09:04 +0200

    create project front-end finished.

--- client/public/src/project/CreateProjectCtrl.js
+++ client/public/src/project/CreateProjectCtrl.js
@@ -9,12 +9,36 @@
                         $scope.teams = data;
                     });
             };
+
+            $scope.project = {};
+            $scope.today = $filter('date')(new Date(), 'yyyy-MM-dd');
+
+            $scope.validateStartDate = function () {
+                if ($scope.project.start_date < $scope.today) {
+                    $scope.createProjectForm.start_date.$setValidity('startBeforeToday', false);
+                } else {
+                    $scope.createProjectForm.start_date.$setValidity('startBeforeToday', true);
+                }
+                $scope.validateEndDate();
+            };
+
+            $scope.validateEndDate = function () {
+                if ($scope.project.end_date < $scope.project.start_date) {
+                    $scope.createProjectForm.end_date.$setValidity('finishBeforeStart', false);
+                } else {
+                    $scope.createProjectForm.end_date.$setValidity('finishBeforeStart', true);
+                }
+            };
+
             $scope.getTeams();
             $scope.createProject = function (project) {
                 console.log(project);
                 project.team = project.team.id;
                 project.board = null;
-                //project.start_date = $filter('date')(theme.start_date_web_training, 'yyyy-MM-dd');
+                console.log(project.start_date);
+                project.start_date = $filter('date')(project.start_date, 'yyyy-MM-ddTHH:mm');
+                console.log(project.start_date);
+                project.end_date = $filter('date')(project.end_date, 'yyyy-MM-ddTHH:mm');
                 ProjectService.createProject(project)
                     .success(function (dataTeam) {
                         console.log(dataTeam);
