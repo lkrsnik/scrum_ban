37386afcec28c872c1526c34fe98c1e2d0d7ab57
Luka <krsnik.luka92@gmail.com>
2015-06-03 12:11:33 +0200

    Added filter on boards - backend

--- server/app/views/board.py
+++ server/app/views/board.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from app.serializers.board import BoardSerializer, ColumnSerializer, \
                                     CardSerializer
-from app.models import Board, Column, Card
+from app.models import Board, Column, Card, Project, UserTeam, Team
 
 
 class BoardViewSet(viewsets.ModelViewSet):
@@ -12,6 +12,21 @@ class BoardViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = BoardSerializer
     queryset = Board.objects.all()
+
+    def list(self, request, pk=None):
+        #Data preparation
+        userTeamQS = UserTeam.objects.filter(user = request.user)
+        #teamQS = Team.objects.filter(id__in = userTeamQS.values('team'))
+        projectQS = Project.objects.filter(team__in = userTeamQS.values('team'))
+        boardQS = Board.objects.filter(id__in = projectQS.values('board'))
+        print ("userTeamQS", userTeamQS)
+        #print ("values", userTeamQS.values('team'))
+        #print ("userTeamQS.name", userTeamQS.team.all())
+        #print ("teamQS", teamQS[2].name)
+        #print ("boardQS", boardQS[2].name)
+        #queryset = Project.objects.filter(team__ = 'Leader')
+        serializer = self.serializer_class(boardQS, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ColumnViewSet(viewsets.ModelViewSet):
